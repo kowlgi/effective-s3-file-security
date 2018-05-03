307254054613788df7a17213d9b200cb8b44c435
rocketsummer <rocketsummer@users.noreply.github.com>
2018-05-03 15:57:05 -0500

    add labels to input boxes

--- app/app.js
+++ app/app.js
@@ -5,18 +5,20 @@ var app = new Vue({
     data: {
         message: 'Hello world',
         navaction: 'Log In',
+        putfile: '',
         putobjectdata: '',
-        getobjectdata: ''
+        getobjectdata: '',
+        getfile: ''
     },
     methods: {
         uploadEncryptedData(){
-            s3en.putObject(aws_config, {Body: this.putobjectdata, Key: 'test', Bucket: 'houston-techfest-bucket', KmsParams: {KeyId: 'arn:aws:kms:us-east-1:311629526619:key/a2b8864e-99f1-4408-ad3a-e7ee2a129fd2', KeySpec: "AES_256"}}, function(data){
+            s3en.putObject(aws_config, {Body: this.putobjectdata, Key: this.putfile, Bucket: 'houston-techfest-bucket', KmsParams: {KeyId: 'arn:aws:kms:us-east-1:311629526619:key/a2b8864e-99f1-4408-ad3a-e7ee2a129fd2', KeySpec: "AES_256"}}, function(data){
                 console.log(data);
             });
         },
         downloadAndDecryptData(){
             const parent = this;
-            s3en.getObject(aws_config, {Key: 'test', Bucket: 'houston-techfest-bucket'}, function(error, data){
+            s3en.getObject(aws_config, {Key: this.getfile, Bucket: 'houston-techfest-bucket'}, function(error, data){
                 console.log(data);
                 parent.getobjectdata = data.Body;
             });
