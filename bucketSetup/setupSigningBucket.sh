396bd3b3b98614bb7c6fb9d26b786b232458ebfa
rocketsummer <rocketsummer@users.noreply.github.com>
2018-05-04 18:52:49 -0500

    remove bucketSetup

--- bucketSetup/setupSigningBucket.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-aws --profile techfest s3 mb s3://signing-demo-bucket && \
-aws --profile techfest s3api put-bucket-policy --bucket signing-demo-bucket --policy file://signing-demo-bucket-policy.json && \
-aws --profile techfest s3api put-bucket-cors --bucket signing-demo-bucket --cors-configuration file://signing-demo-bucket-cors.json
