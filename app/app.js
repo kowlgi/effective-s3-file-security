05a99ac606de57f456322a51197d2ca8bc60207a
rocketsummer <rocketsummer@users.noreply.github.com>
2018-05-04 09:08:10 -0500

    enable and disable decryption/encryption

--- app/app.js
+++ app/app.js
@@ -1,5 +1,8 @@
 var s3en = require('node-s3-encryption-client');
 var aws_config = require('./config.js').aws_config;
+var AWS = require("aws-sdk");
+AWS.config.update(aws_config);
+var s3 = new AWS.S3();
 var app = new Vue({
     el: '#app',
     data: {
@@ -7,23 +10,58 @@ var app = new Vue({
         navaction: 'Log In',
         putfile: '',
         putobjectdata: '',
-        getobjectdata: '',
         getfile: '',
         enableEncryption: false,
-        enableDecryption: false
+        enableDecryption: false,
+        encrypterrormessage: '',
+        encryptsuccessmessage: '',
+        decrypterrormessage: '',
+        decryptsuccessmessage: ''
     },
     methods: {
         uploadEncryptedData(){
-            s3en.putObject(aws_config, {Body: this.putobjectdata, Key: this.putfile, Bucket: 'houston-techfest-bucket', KmsParams: {KeyId: 'arn:aws:kms:us-east-1:311629526619:key/a2b8864e-99f1-4408-ad3a-e7ee2a129fd2', KeySpec: "AES_256"}}, function(data){
-                console.log(data);
-            });
+            const parent = this;
+            this.encryptsuccessmessage = '';
+            this.encrypterrormessage = '';
+            if(this.enableEncryption) {
+                s3en.putObject(aws_config, {Body: this.putobjectdata, Key: this.putfile, Bucket: 'houston-techfest-bucket', KmsParams: {KeyId: 'arn:aws:kms:us-east-1:311629526619:key/a2b8864e-99f1-4408-ad3a-e7ee2a129fd2', KeySpec: "AES_256"}}, function(error, data){
+                    if(error) {
+                        parent.encrypterrormessage = error;
+                    } else {
+                        parent.encryptsuccessmessage = "File upload complete."
+                    }
+                });
+            } else {
+                s3.putObject({Body: this.putobjectdata, Key: this.putfile, Bucket: 'houston-techfest-bucket'}, function(error, data){
+                    if(error) {
+                        parent.encrypterrormessage = error;
+                    } else {
+                        parent.encryptsuccessmessage = "File upload complete."
+                    }
+                });
+            }
         },
         downloadAndDecryptData(){
             const parent = this;
-            s3en.getObject(aws_config, {Key: this.getfile, Bucket: 'houston-techfest-bucket'}, function(error, data){
-                console.log(data);
-                parent.getobjectdata = data.Body;
-            });
+            this.decryptsuccessmessage = '';
+            this.decrypterrormessage = '';
+            if(this.enableDecryption) {
+                s3en.getObject(aws_config, {Key: this.getfile, Bucket: 'houston-techfest-bucket'}, function(error, data){
+                    if(error) {
+                        parent.decrypterrormessage = error;
+                    } else {
+                        parent.decryptsuccessmessage = "File contents: \"" + data.Body + "\"";
+                    }
+                });
+            } else {
+                s3.getObject({Key: this.getfile, Bucket: 'houston-techfest-bucket'}, function(error, data){
+                    if(error) {
+                        parent.decrypterrormessage = error;
+                    } else {
+                        parent.decryptsuccessmessage = "File contents: \"" + data.Body + "\"";
+                    }
+                });
+            }
         }
     }
 });
